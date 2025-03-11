6acb1b26d028bd84da039ab08beb3e16eacb4bb3
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-03-11 17:59:12 +0545

    Update evo-x.sh

--- evo-x.sh
+++ evo-x.sh
@@ -80,7 +80,7 @@ echo ""
 source build/envsetup.sh
 export BUILD_USERNAME=sarojtaj77
 export BUILD_HOSTNAME=T800-machine
-export ALLOW_MISSING_DEPENDENCIES=true; \
-breakfast sapphire user; \
-make installclean; \
-m evolution"
+export ALLOW_MISSING_DEPENDENCIES=true
+breakfast sapphire user
+make installclean
+m evolution
