f18428be1eece18d819941bc7262456bfa36bf5c
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-04-14 14:44:07 +0545

    Update los-22.sh

--- los-22.sh
+++ los-22.sh
@@ -120,7 +120,7 @@ echo "============================"
 
 # Upload ROM zip file to PixelDrain
 ROM_DIR="out/target/product/sapphire/"
-ROM_NAME=$(ls $ROM_DIR | grep "lineage-22.2-.*-UNOFFICIAL-sapphire.zip" | tail -n 1)
+ROM_NAME=$(ls $ROM_DIR | grep "lineage-22.2-.*-UNOFFICIAL-sapphire.zip$" | tail -n 1) # Strictly match .zip files
 
 if [ -n "$ROM_NAME" ]; then
     ROM_PATH="$ROM_DIR$ROM_NAME"
