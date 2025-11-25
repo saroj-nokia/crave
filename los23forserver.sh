f62c3c6c59138ec527b17f625f79ca47cb31bd8a
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-11-25 19:17:35 +0545

    Update los23forserver.sh

--- los23forserver.sh
+++ los23forserver.sh
@@ -105,7 +105,7 @@ echo "============================"
 
 # Upload ROM zip file to PixelDrain
 ROM_DIR="out/target/product/sapphire/"
-ROM_NAME=$(ls $ROM_DIR | grep "EvolutionX-15.0-.*-sapphire-.*-Unofficial.zip$" | tail -n 1)
+ROM_NAME=$(ls $ROM_DIR | grep "ineage-23.0-.*-UNOFFICIAL-sapphire.zip$" | tail -n 1)
 
 if [ -n "$ROM_NAME" ]; then
     ROM_PATH="$ROM_DIR$ROM_NAME"
