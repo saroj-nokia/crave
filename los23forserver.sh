ecac4facc232b1fee2adf824d6ef4c7bb9aa20cc
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-12-17 11:03:14 +0545

    Update los23forserver.sh

--- los23forserver.sh
+++ los23forserver.sh
@@ -30,7 +30,7 @@ echo "============================"
 echo ""
 
 # Sync the repositories using the Crave sync script
-repo sync --force-sync
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j14
 if [ $? -ne 0 ]; then
     echo "Repo sync failed. Exiting."
     exit 1
