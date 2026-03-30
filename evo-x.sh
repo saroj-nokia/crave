5557b4dd80c90c2288b2da3597b0c3827c214752
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2026-03-30 12:55:22 +0545

    Enhance cleanup and clone modified evo updater
    Added automatic cleanup and cloning of modified evo updater package.

--- evo-x.sh
+++ evo-x.sh
@@ -5,6 +5,7 @@ echo "Performing cleanup..."
 rm -rf .repo/local_manifests/
 rm -rf hardware/qcom-caf/common
 rm -rf vendor/qcom/opensource/healthd-ext
+rm -rf packages/apps/Updater
 echo "Cleanup completed."
 echo ""
 
@@ -38,6 +39,20 @@ if [ $? -ne 0 ]; then
 fi
 echo "============================"
 echo "Repo sync success"
+echo "============================"
+echo ""
+
+# Automatic cleanup
+echo "Performing cleanup..."
+rm -rf packages/apps/Updater
+echo "Cleanup completed."
+echo ""
+
+# Clone modified evo update package
+echo "Clone modified evo updater package"
+git clone https://github.com/sapphire-sm6225/packages_apps_Updater.git -b vic packages/apps/Updater
+echo "============================"
+echo "modified evo update package clone success"
 echo "============================"
 echo ""
 
