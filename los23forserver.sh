a7a95dd7248801c22bc4f381879f8ca5c649df01
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2026-05-19 09:10:08 +0545

    Update branches for git clone commands in script
    Updated git clone commands to use lineage-23.2-caf-sm6225 branch for data-ipa-cfg-mgr, dataipa, and media.

--- los23forserver.sh
+++ los23forserver.sh
@@ -55,16 +55,16 @@ rm -rf hardware/qcom-caf/sm6225/audio/pal
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_arpal-lx.git hardware/qcom-caf/sm6225/audio/pal
 
 rm -rf hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_data-ipa-cfg-mgr.git hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_data-ipa-cfg-mgr.git hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
 
 rm -rf hardware/qcom-caf/sm6225/dataipa
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm6225/dataipa
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm6225/dataipa
 
 rm -rf hardware/qcom-caf/sm6225/display
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_display.git hardware/qcom-caf/sm6225/display
 
 rm -rf hardware/qcom-caf/sm6225/media
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_media.git hardware/qcom-caf/sm6225/media
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_media.git hardware/qcom-caf/sm6225/media
 
 rm -rf hardware/qcom-caf/sm6225/audio/primary-hal
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_audio.git hardware/qcom-caf/sm6225/audio/primary-hal
