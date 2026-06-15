acb278493c0e37667d10f948f398a6648e4ac31c
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2026-06-15 12:49:42 +0545

    Update los23.sh

--- los23.sh
+++ los23.sh
@@ -4,10 +4,7 @@
 echo "Performing cleanup..."
 rm -rf .repo/local_manifests/
 rm -rf hardware/qcom-caf/common
-rm -rf packages/apps/ThemePicker
-rm -rf vendor/qcom/opensource/healthd-ext
 rm -rf vendor/lineage
-rm -rf packages/modules/Permission
 echo "Cleanup completed."
 echo ""
 
@@ -56,25 +53,22 @@ rm -rf hardware/qcom-caf/sm6225/audio/pal
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_arpal-lx.git hardware/qcom-caf/sm6225/audio/pal
 
 rm -rf hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_data-ipa-cfg-mgr.git hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_data-ipa-cfg-mgr.git hardware/qcom-caf/sm6225/data-ipa-cfg-mgr
 
 rm -rf hardware/qcom-caf/sm6225/dataipa
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm6225/dataipa
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/vendor_qcom_opensource_dataipa.git hardware/qcom-caf/sm6225/dataipa
 
 rm -rf hardware/qcom-caf/sm6225/display
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_display.git hardware/qcom-caf/sm6225/display
 
 rm -rf hardware/qcom-caf/sm6225/media
-git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_media.git hardware/qcom-caf/sm6225/media
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_media.git hardware/qcom-caf/sm6225/media
 
 rm -rf hardware/qcom-caf/sm6225/audio/primary-hal
 git clone --depth 1 -b lineage-22.0-caf-sm6225 https://github.com/sapphire-sm6225/hardware_qcom_audio.git hardware/qcom-caf/sm6225/audio/primary-hal
 
 rm -rf device/qcom/sepolicy_vndr/sm6225
-git clone --depth 1 -b lineage-23.0-caf-sm6225 https://github.com/sapphire-sm6225/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm6225
-
-rm -rf vendor/qcom/opensource/healthd-ext
-git clone --depth 1 -b lineage-23.2 https://github.com/sapphire-sm6225/android_vendor_qcom_opensource_healthd-ext.git vendor/qcom/opensource/healthd-ext
+git clone --depth 1 -b lineage-23.2-caf-sm6225 https://github.com/sapphire-sm6225/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm6225
 echo "============================"
 echo "Cloning HALs completed"
 echo "============================"
@@ -82,17 +76,8 @@ echo ""
 
 # Automatic cleanup
 echo "Performing cleanup..."
-rm -rf packages/apps/ThemePicker
 rm -rf vendor/lineage
 echo "Cleanup completed."
-echo ""
-
-# Clone modified lineage ThemePicke repo
-echo "Clone modified lineage ThemePicker repo"
-git clone https://github.com/sapphire-sm6225/android_packages_apps_ThemePicker -b lineage-23.2 packages/apps/ThemePicker
-echo "============================"
-echo "modified lineage ThemePicker repo clone success"
-echo "============================"
 echo ""
 
 # Clone modified lineage vendor
